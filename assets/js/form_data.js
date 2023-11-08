cf61b95d17ab3a243e5ac121339bd3482dfd7455
Benjamin Owen <benjamin@projecttiy.com>
2023-11-08 13:34:25 -0500

    attempting a different way of clearing text

--- assets/js/form_data.js
+++ assets/js/form_data.js
@@ -12,13 +12,11 @@ function get_incident_title() {
             success: function(data) {
                 var arr = JSON.parse(data);
                 if(arr.platform_name != "None") {
-                    $("#incident-alert-text").html();
-                    $("#incident-alert").removeClass("alert-danger");
+                    reset_alert_box("incident");
                     $("#incident-alert").addClass("alert-success");
                     $("#incident-alert-text").text(arr.platform_name + ": " + title);
                 } else {
-                    $("#incident-alert-text").html();
-                    $("#incident-alert").removeClass("alert-success");
+                    reset_alert_box("incident");
                     $("#incident-alert").addClass("alert-danger");
                     $("#incident-alert-text").text("Error getting platform name!");
                 }
@@ -30,8 +28,7 @@ function get_incident_title() {
 function get_incident_message() {
     $(document).ready(function() {
         var message = $("#incident-message").val();
-        $("#incident-alert-text").html();
-        $("#incident-alert").removeClass("alert-danger");
+        reset_alert_box("incident");
         $("#incident-alert").addClass("alert-success");
         $("#incident-alert-text").text(message);
     });
@@ -51,13 +48,11 @@ function get_maintenance_title() {
             success: function(data) {
                 var arr = JSON.parse(data);
                 if(arr.platform_name != "None") {
-                    $("#maintenance-alert-text").html();
-                    $("#maintenance-alert").removeClass("alert-danger");
+                    reset_alert_box("maintenance");
                     $("#maintenance-alert").addClass("alert-success");
                     $("#maintenance-alert-text").text(arr.platform_name + ": " + title);
                 } else {
-                    $("#maintenance-alert-text").html();
-                    $("#maintenance-alert").removeClass("alert-success");
+                    reset_alert_box("maintenance");
                     $("#maintenance-alert").addClass("alert-danger");
                     $("#maintenance-alert-text").text("Error getting platform name!");
                 }
@@ -82,14 +77,12 @@ function get_maintenance_details() {
             success: function(data) {
                 var arr = JSON.parse(data);
                 if(arr.ticket_status == 1 && arr.ref_status == 1) {
-                    $("#maintenance-alert-text").html();
-                    $("#maintenance-alert").removeClass("alert-danger");
+                    reset_alert_box("maintenance");
                     $("#maintenance-alert").addClass("alert-success");
                     var formatted_message = build_maintenance_message(ticket, message, ref_link);
                     $("#maintenance-alert").text(formatted_message);
                 } else {
-                    $("#maintenance-alert-text").html();
-                    $("#maintenance-alert").removeClass("alert-success");
+                    reset_alert_box("maintenance");
                     $("#maintenance-alert").addClass("alert-danger");
                     $("#maintenance-alert-text").text("Ticket Number of Reference Link are invalid!");
                 }
@@ -105,4 +98,16 @@ function build_maintenance_message(ticket, message, ref) {
     var message = message + '<p class="reference_link">' + ref + '</p>';
     var message = message + '</div>';
     return message;
+}
+
+function reset_alert_box(name) {
+    if(name == "incident") {
+        $("#incident-alert").removeClass("alert-danger");
+        $("#incident-alert").removeClass("alert-success");
+        $("#incident-alert").html('<span id="incident-alert-text"></span>');
+    } else if(name == "maintenance") {
+        $("#maintenance-alert").removeClass("alert-danger");
+        $("#maintenance-alert").removeClass("alert-success");
+        $("#maintenance-alert").html('<span id="maintenance-alert-text"></span>');
+    }
 }
