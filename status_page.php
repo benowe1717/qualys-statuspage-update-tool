7d913b6102c0f93ade28b8fccb7e6d29fa6f9ac7
Benjamin Owen <benjamin@projecttiy.com>
2023-11-09 14:30:23 -0500

    importing montserrat font and adding a favicon

--- status_page.php
+++ status_page.php
@@ -7,6 +7,7 @@
     <title>Status Page Update</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/styles.min.css">
+    <link rel="stylesheet" href="/assets/css/montserrat-font.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
     <script src="/assets/js/reset_form.js"></script>
