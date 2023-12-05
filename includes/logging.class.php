41bc954fc85aa788a4aff499f2cb362de690a0f7
Benjamin Owen <benjamin@projecttiy.com>
2023-12-05 16:03:53 -0500

    only the error_log() function seems to work in docker containers

--- includes/logging.class.php
+++ includes/logging.class.php
@@ -2,26 +2,26 @@
 
     class my_logger {
 
-        private $file = __DIR__ . "/.creds.ini";
         private $date_format = "M d H:m:s";
         private $hostname = "";
         private $program = "php";
         private $pid = -1;
-        public $fp;
         public $error;
 
         function __construct() {
-            if($this->fp = fopen("php://stdout", "w")) {
-                $this->hostname = gethostname();
-                $this->pid = getmypid();
-                $this->log_msg("Starting logger...");
-            }
+            $this->hostname = gethostname();
+            $this->pid = getmypid();
+            $this->log_msg("Starting logger...");
         }
 
         public function log_msg(string $msg) {
-            $now = date($this->date_format);
-            $message = "{$now} {$this->hostname} {$this->program}[{$this->pid}] {$msg}";
-            fwrite($this->fp, $message);
+            try{
+                $now = date($this->date_format);
+                $message = "{$now} {$this->hostname} {$this->program}[{$this->pid}] {$msg}";
+                error_log($message);
+            } catch(Exception $e) {
+                file_put_contents("/tmp/error", $e, FILE_APPEND | LOCK_EX);
+            }
         }
 
     }
